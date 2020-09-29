00d30313bed391c9ace68a72b429ce7c3ffdbbe5
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2020-09-29 22:34:47 +0300

    Сделано заполнение дом элемента на осн массива

--- js/setup.js
+++ js/setup.js
@@ -1,11 +1,13 @@
 'use strict';
-const setup = document.querySelector(`.setup`);
-setup.classList.remove(`hidden`);
 const NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
 const SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const COATS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const EYES = [`black`, `red`, `blue`, `yellow`, `green`];
 const WIZARDS_AMOUNT = 4;
+
+const setup = document.querySelector(`.setup`);
+setup.classList.remove(`hidden`);
+const wizardTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
 
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
@@ -13,13 +15,13 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min; // Максимум не включается, минимум включается
 };
 
-// заполнения блока DOM-элементами на основе массива JS-объектов.
-// Создайте массив, состоящий из 4-х сгенерированных JS объектов, которые будут описывать похожих персонажей. Объекты должны содержать следующие поля:
-//
-// name, строка — случайно сгенерированное имя персонажа. Имя генерируется из массивов имён и фамилий: нужно случайным образом выбрать из массива имён имя,
-// а из массива фамилий фамилию и сложить их. При желании имя и фамилию можно в случайном порядке менять местами:
+// Стоит отдельно объявить
+// 1)функцию генерации случайных данных,
+// 2)функцию создания DOM-элемента на основе JS-объекта,
+// 3)функцию заполнения блока DOM-элементами на основе массива JS-объектов.
 
-const createwizardsArray = () => {
+
+const createWizardsArray = () => {
   const wizardsData = [];
   for (let i = 0; i <= WIZARDS_AMOUNT; i++) {
     wizardsData[i] = {
@@ -30,3 +32,13 @@ const createwizardsArray = () => {
   }
   return wizardsData;
 };
+const wizardsData = createWizardsArray();
+
+const createWizard = (wizard) => {
+  const wizardElement = wizardTemplate.cloneNode(true);
+  wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
+  wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
+  wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
